4db8604d0492fb7553d76ae0e115e1f44329c95e
Dhruvik <dhruviksparikh@gmail.com>
2022-11-07 22:43:41 +0530

    Added changes

---
+++ src/test/java/com/simplilearn/REST_DATA_JPA_07112022/RestDataJpa07112022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.REST_DATA_JPA_07112022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestDataJpa07112022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
